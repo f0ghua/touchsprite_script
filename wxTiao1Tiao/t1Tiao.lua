d9980462bb403b643d66b7f004a75cecd7b8da75
fog <fog.hua@gmail.com>
2018-01-04 15:24:14 +0800

    ! change alg for locate the top center point of block

--- wxTiao1Tiao/t1Tiao.lua
+++ wxTiao1Tiao/t1Tiao.lua
@@ -3,6 +3,9 @@ Description:
 
 --]]
 
+local LOG_FILE_NAME = "ts";
+local POINT_NUM_PERLINE = 20;
+local QIZI_IMAGE_WIDTH = 32;
 local version = 'v1.0.01'
 local _screen_w, _screen_h;
 local _ht_x, _ht_y, _ht_c, _hb_x, _hb_y, _hb_c, _cs;
@@ -74,6 +77,13 @@ function _multi_color(array,s)
     return true
 end
 
+function _toast_printf(s, ...)
+	return toast(s:format(...));
+end
+
+function _log_printf(s, ...)
+	return wLog(LOG_FILE_NAME, s:format(...));
+end
 
 init(0);
 mSleep(1000);
@@ -83,7 +93,7 @@ mSleep(1000);
 --]]
 
 p_init();
-initLog("test", 0);
+initLog(LOG_FILE_NAME, 0);
 
 --x, y, x1, y1, x2, y2, x3, y3;
 --c1, c2, c3;
@@ -108,12 +118,12 @@ end
 
 local x_qz, y_qz = findImage("qizi.png", 0, 0, _screen_w, _screen_h);
 if x_qz ~= -1 and y_qz ~= -1 then
-    toast("image: "..x_qz..","..y_qz);
+	_toast_printf("image %d, %d", x_qz, y_qz);
 else
     toast('picture is not found!');        
 end
 
-local x_qzC = x_qz + 16;
+local x_qzC = x_qz + QIZI_IMAGE_WIDTH/2;
 local x_from, x_to;
 if x_qz < _screen_w/2 then
 	x_from, x_to = 320, 550;
@@ -125,7 +135,6 @@ r2, g2, b2 = getColorRGB(x2, y2);
 c_r = (r2 + r1)/2;
 c_g = (g2 + g1)/2;
 c_b = (b2 + b1)/2;
---wLog("test", "c_r = "..c_r..", c_g = "..c_g..", c_b = "..c_b);
 
 d_r = math.abs((r2 - r1)/2);
 d_g = math.abs((g2 - g1)/2);
@@ -133,36 +142,57 @@ d_b = math.abs((b2 - b1)/2);
 
 local x_block_c, y_block_c;
 
-for j=400,500 do
+-- first scan, to find the block for next step
+local y_1stScan;
+for y = 400, 500 do
 	local bk = false;
-	for i=x_from,x_to,3 do
-	--for x=0,_screen_w-1,3 do
-		if _is_color_rgb(i, j, c_r, c_g, c_b, d_r, d_g, d_b)~=true then
-			toast("found: "..i..","..j); 
-			x_block_c, y_block_c = i, j;
+	for x = 0, _screen_w-1, (_screen_w)/10 do
+		if _is_color_rgb(x, y, c_r, c_g, c_b, d_r, d_g, d_b)~=true then
+			_toast_printf("found point y = %d", y);
+			y_1stScan = y;
 			bk = true;
 			break;
 		end
-	end
+	end	
 	if bk == true then
 		break;
 	end
 end
+
+-- second scan, to find the top left point of the block
+for y = y_1stScan-5, y_1stScan+5 do
+	local bk = false;
+	for x = 0, _screen_w-1, 3 do
+		if _is_color_rgb(x, y, c_r, c_g, c_b, d_r, d_g, d_b)~=true then
+			_toast_printf("found point x = %d, y = %d", x, y);
+			x_block_c, y_block_c = x, y;
+			bk = true;
+			break;
+		end
+	end	
+	if bk == true then
+		break;
+	end
+end
+
+-- third scan, to find the top center point of the block
+local x;
 x = x_block_c;
 x1 = x;
 while _is_color_rgb(x, y_block_c, c_r, c_g, c_b, d_r, d_g, d_b)~=true do
 	x = x + 1;
 end
 x_block_c = math.floor((x+x1)/2);
+_toast_printf("x = %d, x1 = %d, x_block_c = %d", x, x1, x_block_c);
 
+-- lua_exit(); 
 
 local len = math.abs(x_qzC - x_block_c)/math.sqrt(3)*2;
-toast("len = "..math.floor(len)..", "..x_qzC..", "..x_block_c);
---wLog("test", "x = "..x..", x1 = "..x1..", x_block_c = "..x_block_c);
+_toast_printf("len, x_qzC, x_blockC = %d, %d, %d", len, x_qzC, x_block_c);
 
--- touchDown(x_qzC, y_qz);
--- mSleep(len*2.35);
--- touchUp(x_qzC, y_qz);
+touchDown(x_qzC, y_qz);
+mSleep(len*2.35);
+touchUp(x_qzC, y_qz);
 
 
 
